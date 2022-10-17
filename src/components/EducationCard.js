e03c26d14693ca66f9b8209e9430ddc57536beb6
budy6991 <alvmenfer@gmail.com>
2022-10-18 00:18:17 +0300

    Displayed functional component for EducationCard

--- src/components/EducationCard.js
+++ src/components/EducationCard.js
@@ -1,144 +1,112 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export class EducationCard extends Component {
-  constructor(props) {
-    super(props);
+function EducationCard({
+  degree,
+  university,
+  date,
+  id,
+  handleEdit,
+  handleRemove,
+}) {
+  const [newDegree, setNewDegree] = useState(degree);
+  const [newUniversity, setNewUniversity] = useState(university);
+  const [newDate, setNewDate] = useState(date);
+  const [isHovering, setIsHovering] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    this.state = {
-      editing: null,
-      newDegree: this.props.degree,
-      newUniversity: this.props.university,
-      newDate: this.props.date,
-      isHovering: false,
-      isEditing: false,
-    };
-  }
-
-  handleMouseOver = () => {
-    this.setState({
-      isHovering: true,
-    });
+  const handleMouseOver = () => {
+    setIsHovering(true);
   };
 
-  handleMouseOut = () => {
-    this.setState({
-      isHovering: false,
-    });
+  const handleMouseOut = () => {
+    setIsHovering(false);
   };
 
-  startEditing = () => {
-    this.setState({
-      isEditing: true,
-    });
+  const startEditing = () => {
+    setIsEditing(true);
   };
 
-  stopEditing = () => {
-    this.setState({
-      isEditing: false,
-    });
-    this.props.handleEdit(this.state, this.props.id);
+  const stopEditing = () => {
+    setIsEditing(false);
+    handleEdit({ newDegree, newUniversity, newDate }, id);
   };
 
-  handleDegreeEdit = (event) => {
-    this.setState({
-      newDegree: event.target.value,
-    });
+  const handleDegreeEdit = (event) => {
+    setNewDegree(event.target.value);
   };
 
-  handleUniversityEdit = (event) => {
-    this.setState({
-      newUniversity: event.target.value,
-    });
+  const handleUniversityEdit = (event) => {
+    setNewUniversity(event.target.value);
   };
 
-  handleDateEdit = (event) => {
-    this.setState({
-      newDate: event.target.value,
-    });
+  const handleDateEdit = (event) => {
+    setNewDate(event.target.value);
   };
 
-  showButtons = () => {
+  if (isHovering === null || isHovering === false) {
     return (
-      <div>
-        <button className="rounded-full bg-white p-1 hover:shadow-md hover:bg-blue-600 hover:text-white">
-          Edit
-        </button>
-        <button className="rounded-full bg-white p-1 hover:shadow-md hover:bg-red-600 hover:text-white">
-          Remove
-        </button>
+      <div
+        className=" p-4 flex justify-between bg-white shadow-md shadow-blue-700 rounded-2xl my-2 items-center"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        <div className="text-xs font-bold">{degree}</div>
+        <div className="text-xs">{university}</div>
+        <div className="text-xs text-blue-500">{date}</div>
       </div>
     );
-  };
-
-  render() {
-    const { degree, university, date } = this.props;
-
-    const { isHovering } = this.state;
-    if (isHovering === null || isHovering === false) {
-      return (
-        <div
-          className=" p-4 flex justify-between bg-white shadow-md shadow-blue-700 rounded-2xl my-2 items-center"
-          onMouseOver={this.handleMouseOver}
-          onMouseOut={this.handleMouseOut}
-        >
-          <div className="text-xs font-bold">{degree}</div>
-          <div className="text-xs">{university}</div>
-          <div className="text-xs text-blue-500">{date}</div>
-        </div>
-      );
-    } else {
-      return (
-        <div
-          className=" p-4 flex justify-between bg-white shadow-lg shadow-blue-700 rounded-2xl my-2 items-center"
-          onMouseOver={this.handleMouseOver}
-          onMouseOut={this.handleMouseOut}
-        >
-          {this.state.isEditing === true ? (
-            <div>
-              <input
-                value={this.state.newDegree}
-                onChange={this.handleDegreeEdit}
-                className="text-xs font-bold"
-              ></input>
-              <input
-                className="text-xs font-extralight"
-                value={this.state.newUniversity}
-                onChange={this.handleUniversityEdit}
-              ></input>
-              <input
-                className="text-xs text-blue-500"
-                value={this.state.newDate}
-                onChange={this.handleDateEdit}
-              ></input>
-              <button
-                className="rounded-full bg-white p-1 hover:shadow-md hover:bg-blue-600 hover:text-white text-xs"
-                onClick={this.stopEditing}
-              >
-                Add
-              </button>
-            </div>
-          ) : (
-            <div>
-              <div className="text-xs font-bold">{degree}</div>
-              <div className="text-xs font-extralight">{university}</div>
-              <div className="text-xs text-blue-500">{date}</div>
-              <button
-                className="rounded-full bg-white p-1 hover:shadow-md hover:bg-blue-600 hover:text-white text-xs"
-                onClick={() => this.startEditing()}
-              >
-                Edit
-              </button>
-              <button
-                className="rounded-full bg-white p-1 hover:shadow-md hover:bg-red-600 hover:text-white text-xs"
-                onClick={() => this.props.handleRemove(this.props.id)}
-              >
-                Remove
-              </button>
-            </div>
-          )}
-        </div>
-      );
-    }
+  } else {
+    return (
+      <div
+        className=" p-4 flex justify-between bg-white shadow-lg shadow-blue-700 rounded-2xl my-2 items-center"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        {isEditing === true ? (
+          <div>
+            <input
+              value={newDegree}
+              onChange={handleDegreeEdit}
+              className="text-xs font-bold"
+            ></input>
+            <input
+              className="text-xs font-extralight"
+              value={newUniversity}
+              onChange={handleUniversityEdit}
+            ></input>
+            <input
+              className="text-xs text-blue-500"
+              value={newDate}
+              onChange={handleDateEdit}
+            ></input>
+            <button
+              className="rounded-full bg-white p-1 hover:shadow-md hover:bg-blue-600 hover:text-white text-xs"
+              onClick={stopEditing}
+            >
+              Add
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div className="text-xs font-bold">{degree}</div>
+            <div className="text-xs font-extralight">{university}</div>
+            <div className="text-xs text-blue-500">{date}</div>
+            <button
+              className="rounded-full bg-white p-1 hover:shadow-md hover:bg-blue-600 hover:text-white text-xs"
+              onClick={() => startEditing()}
+            >
+              Edit
+            </button>
+            <button
+              className="rounded-full bg-white p-1 hover:shadow-md hover:bg-red-600 hover:text-white text-xs"
+              onClick={() => handleRemove(id)}
+            >
+              Remove
+            </button>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
